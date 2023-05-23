ce533836a14b093cc86c4f39bbff3d3a6c35c37c
baasankhuu-p <purevbaasankhuu@gmail.com>
2023-05-23 15:26:19 +0800

    3.26

--- src/app/screens/about.js
+++ src/app/screens/about.js
@@ -23,19 +23,19 @@ export default function About() {
       id="about"
       className="relative flex flex-col justify-center items-center mt-10 about-bg"
     >
-        <div className="flex flex-col md:flex-row justify-between lg:justify-center items-center my-5 lg:mx-52  lg:w-5/6">
-          <div className="flex flex-col border-2 mr-2 lg:w-1/3 mx-2">
-            <div className=" text-white border-b-2 text-center p-1 text-3xl md:4xl font-medium flex flex-col gap-4 border-2 py-3 px-6 ">
+      <div className="flex flex-col md:flex-row justify-between lg:justify-center items-center my-5 lg:mx-52  lg:w-5/6">
+        <div className="flex flex-col mr-2 lg:w-1/3 mx-2">
+          <div className=" text-white text-center p-1 text-3xl md:4xl font-medium flex flex-col gap-4 py-3 px-6" style={{borderWidth:1}}>
             About us
-            </div>
-            <div className=" text-white text-center p-1 font-medium flex flex-col gap-4 border-2 py-3 px-6 text-sm sm:flex-row sm:items-center xl:gap-44 xl:py-6 xl:px-11 bg-gradient-to-r">
-              {shortIntro}
-            </div>
           </div>
-          <div className="text-white ml-2 lg:w-2/3 my-5 ">
-            <Deed />
+          <div className=" text-white text-center p-1 font-medium flex flex-col gap-4 border-2 py-3 px-6 text-sm sm:flex-row sm:items-center xl:gap-44 xl:py-6 xl:px-11 bg-gradient-to-r" style={{borderWidth:1}}>
+            {shortIntro}
           </div>
         </div>
+        <div className="text-white ml-2 lg:w-2/3 my-5 ">
+          <Deed />
+        </div>
+      </div>
     </section>
   );
 }
